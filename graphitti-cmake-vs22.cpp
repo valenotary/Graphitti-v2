a082abc383f953074000f4304e008167021c917c
Franz Anthony Varela <valenotary@gmail.com>
2021-12-17 02:00:47 -0800

    Add project files.

---
+++ graphitti-cmake-vs22.cpp
@@ -0,0 +1,11 @@
+﻿// graphitti-cmake-vs22.cpp : Defines the entry point for the application.
+//
+
+#include "graphitti-cmake-vs22.h"
+#include <sqlite3.h>
+
+int main()
+{
+	std::cout << sqlite3_libversion() << '\n';
+	return 0;
+}
