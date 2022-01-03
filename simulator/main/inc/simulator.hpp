d252f62cec9280c8373ad7570e372c68d9da98ce
Franz Anthony Varela <valenotary@gmail.com>
2022-01-02 23:46:22 -0800

    stuff

---
+++ simulator/main/inc/simulator.hpp
@@ -0,0 +1,17 @@
+/**
+ *	@file simulator.hpp
+ *
+ *	@brief Graphitti Simulator static class.
+ *
+ *	@ingroup Graphitti/Core/
+ *
+ */
+
+namespace graphitti {
+    namespace core {
+        class Simulator final { // no one should try to inherit from this...
+            private:
+				Simulator() = delete;
+        };
+    }
+}
